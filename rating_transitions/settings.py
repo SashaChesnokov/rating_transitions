6dd177e6d547e86e7c33530a32523312e0233dc6
SashaChesnokov <chesnokov-an-2017@mail.ru>
2025-04-04 13:58:36 +0300

    Add index page

--- rating_transitions/settings.py
+++ rating_transitions/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'first',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'rating_transitions.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'first/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
